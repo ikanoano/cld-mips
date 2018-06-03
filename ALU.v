fac813837ba9d042c560ecac32d6514f92b0c277
ikanoano <ikanoano@users.noreply.github.com>
2018-06-03 18:44:14 +0900

    predecode alu selector

--- ALU.v
+++ ALU.v
@@ -14,6 +14,19 @@ module ALU (
   output  wire[32-1:0]  rslt
 );
 
+localparam[4-1:0]
+  SEL_ADD =  0,
+  SEL_SUB =  1,
+  SEL_AND =  2,
+  SEL_OR  =  3,
+  SEL_XOR =  4,
+  SEL_NOR =  5,
+  SEL_SLL =  6,
+  SEL_SRL =  7,
+  SEL_SRA =  8,
+  SEL_SLT =  9,
+  SEL_SLTU= 10;
+
 wire[32-1:0]  imm_s     = {{16{imm[15]}}, imm};
 wire[32-1:0]  imm_z     = {{16{   1'b0}}, imm};
 wire[32-1:0]  rrt       =
@@ -29,10 +42,8 @@ wire[ 5-1:0]  shamt     = funct[2] ? rrs[4:0] : shamt_in;
 reg [32-1:0]  rslt_add, rslt_sub, rslt_and, rslt_or, rslt_xor,
               rslt_sll, rslt_srl, rslt_sra, rslt_slt, rslt_sltu;
 
-reg [ 6-1:0]  r_opcode=0, r_funct=0;
+reg [ 4-1:0]  rslt_sel=0;
 always @(posedge clk) begin
-  r_opcode    <= rst ? 0 : opcode;
-  r_funct     <= rst ? 0 : funct;
   rslt_add    <= rst ? 0 : rrs + rrt;
   rslt_sub    <= rst ? 0 : rrs - rrt;
   rslt_and    <= rst ? 0 : rrs & rrt;
@@ -40,42 +51,57 @@ always @(posedge clk) begin
   rslt_xor    <= rst ? 0 : rrs ^ rrt;
   rslt_sll    <= rst ? 0 : rrt << shamt;
   rslt_srl    <= rst ? 0 : rrt >> shamt;
-  rslt_sra    <= rst ? 0 : $signed(rrt) >>>shamt;
+  rslt_sra    <= rst ? 0 : $signed(rrt) >>> shamt;
   rslt_slt    <= rst ? 0 : $signed(rrs) < $signed(rrt) ? 32'b1 : 32'b0;
   rslt_sltu   <= rst ? 0 :         rrs  <         rrt  ? 32'b1 : 32'b0;
+  rslt_sel    <=
+    opcode==`INST_I_ADDI  ||
+    opcode==`INST_I_ADDIU                     ? SEL_ADD :
+    opcode==`INST_I_ANDI                      ? SEL_AND :
+    opcode==`INST_I_ORI                       ? SEL_OR  :
+    opcode==`INST_I_XORI                      ? SEL_XOR :
+    opcode==`INST_I_SLTI                      ? SEL_SLT :
+    opcode==`INST_I_SLTIU                     ? SEL_SLTU:
+//  opcode==`INST_I_LB    ||
+//  opcode==`INST_I_LH    ||
+//  opcode==`INST_I_LBU   ||
+//  opcode==`INST_I_LHU   ||
+    opcode==`INST_I_LW    ||
+//  opcode==`INST_I_SB    ||
+//  opcode==`INST_I_SH    ||
+    opcode==`INST_I_SW                        ? SEL_ADD :
+    opcode==`INST_R && funct==`FUNCT_ADD  ||
+    opcode==`INST_R && funct==`FUNCT_ADDU     ? SEL_ADD :
+    opcode==`INST_R && funct==`FUNCT_SUB  ||
+    opcode==`INST_R && funct==`FUNCT_SUBU     ? SEL_SUB :
+    opcode==`INST_R && funct==`FUNCT_AND      ? SEL_AND :
+    opcode==`INST_R && funct==`FUNCT_OR       ? SEL_OR  :
+    opcode==`INST_R && funct==`FUNCT_XOR      ? SEL_XOR :
+    opcode==`INST_R && funct==`FUNCT_NOR      ? SEL_NOR :
+    opcode==`INST_R && funct==`FUNCT_SLL      ? SEL_SLL :
+    opcode==`INST_R && funct==`FUNCT_SRL      ? SEL_SRL :
+    opcode==`INST_R && funct==`FUNCT_SRA      ? SEL_SRA :
+    opcode==`INST_R && funct==`FUNCT_SLLV     ? SEL_SLL :
+    opcode==`INST_R && funct==`FUNCT_SRLV     ? SEL_SRL :
+    opcode==`INST_R && funct==`FUNCT_SRAV     ? SEL_SRA :
+    opcode==`INST_R && funct==`FUNCT_SLT      ? SEL_SLT :
+    opcode==`INST_R && funct==`FUNCT_SLTU     ? SEL_SLTU:
+                                                4'hX;
+
 end
 
 assign  rslt =
-  r_opcode==`INST_I_ADDI  ||
-  r_opcode==`INST_I_ADDIU                       ? rslt_add :
-  r_opcode==`INST_I_ANDI                        ? rslt_and :
-  r_opcode==`INST_I_ORI                         ? rslt_or  :
-  r_opcode==`INST_I_XORI                        ? rslt_xor :
-  r_opcode==`INST_I_SLTI                        ? rslt_slt :
-  r_opcode==`INST_I_SLTIU                       ? rslt_sltu:
-//r_opcode==`INST_I_LB    ||
-//r_opcode==`INST_I_LH    ||
-//r_opcode==`INST_I_LBU   ||
-//r_opcode==`INST_I_LHU   ||
-  r_opcode==`INST_I_LW    ||
-//r_opcode==`INST_I_SB    ||
-//r_opcode==`INST_I_SH    ||
-  r_opcode==`INST_I_SW                          ? rslt_add :
-  r_opcode==`INST_R && r_funct==`FUNCT_ADD  ||
-  r_opcode==`INST_R && r_funct==`FUNCT_ADDU     ? rslt_add :
-  r_opcode==`INST_R && r_funct==`FUNCT_SUB  ||
-  r_opcode==`INST_R && r_funct==`FUNCT_SUBU     ? rslt_sub :
-  r_opcode==`INST_R && r_funct==`FUNCT_AND      ? rslt_and :
-  r_opcode==`INST_R && r_funct==`FUNCT_OR       ? rslt_or  :
-  r_opcode==`INST_R && r_funct==`FUNCT_XOR      ? rslt_xor :
-  r_opcode==`INST_R && r_funct==`FUNCT_NOR      ?~rslt_or  :
-  r_opcode==`INST_R && r_funct==`FUNCT_SLL      ? rslt_sll :
-  r_opcode==`INST_R && r_funct==`FUNCT_SRL      ? rslt_srl :
-  r_opcode==`INST_R && r_funct==`FUNCT_SRA      ? rslt_sra :
-  r_opcode==`INST_R && r_funct==`FUNCT_SLLV     ? rslt_sll :
-  r_opcode==`INST_R && r_funct==`FUNCT_SRLV     ? rslt_srl :
-  r_opcode==`INST_R && r_funct==`FUNCT_SRAV     ? rslt_sra :
-  r_opcode==`INST_R && r_funct==`FUNCT_SLT      ? rslt_slt :
-  r_opcode==`INST_R && r_funct==`FUNCT_SLTU     ? rslt_sltu: 32'hXXXX;
+  rslt_sel==SEL_ADD   ? rslt_add :
+  rslt_sel==SEL_SUB   ? rslt_sub :
+  rslt_sel==SEL_AND   ? rslt_and :
+  rslt_sel==SEL_OR    ? rslt_or  :
+  rslt_sel==SEL_XOR   ? rslt_xor :
+  rslt_sel==SEL_NOR   ? ~rslt_or :
+  rslt_sel==SEL_SLL   ? rslt_sll :
+  rslt_sel==SEL_SRL   ? rslt_srl :
+  rslt_sel==SEL_SRA   ? rslt_sra :
+  rslt_sel==SEL_SLT   ? rslt_slt :
+  rslt_sel==SEL_SLTU  ? rslt_sltu:
+                        32'hXXXX;
 
 endmodule
